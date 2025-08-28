af1ae5b0a054d496512c4d53bc217ec95e6a7097
Ford Henley <info@fordswebhelp.com>
2025-08-28 17:52:01 -0400

    completed exercise two

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program
     {
+        public static void favoriteSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine("Wow, I really struggled with math in school!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science was my favorite subject too!");
+                    break;
+                case "History":
+                    Console.WriteLine("We should all love history so we don't repeat it!");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           favoriteSubject();
         }
     }
 }
